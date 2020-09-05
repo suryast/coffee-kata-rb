ead41d207f273dced4e8538d5fdd3d454397dc9a
Surya Setiyaputra <surya.setiyaputra@online-pajak.com>
2020-09-05 17:54:02 +1000

    first commit

---
+++ lib/coffee_cart.rb
@@ -0,0 +1,27 @@
+# typed: true
+# define class CoffeeCart
+
+# Implementation of CoffeeCart
+class CoffeeCart
+  extend T::Sig
+
+  sig { params(drink: String, sugar: Integer).returns(String) }
+
+  def receive_order(drink, sugar)
+    stick = T.must(sugar).positive? ? 0 : ''
+    sugar = sugar == 0 ? '' : sugar
+
+    # return
+    drink + ':' + sugar.to_s + ':' + stick.to_s
+  end
+
+
+  sig { params(_message: String).returns(String) }
+
+  def deliver_message(_message)
+    full_message = 'M:' + %(#{_message})
+
+    # return
+    full_message
+  end
+end
---
+++ spec/coffe_cart_spec.rb
@@ -0,0 +1,61 @@
+# typed: false
+# spec/test_spec.rb
+
+require 'sorbet-runtime'
+
+require 'coffee_cart'
+
+describe CoffeeCart do
+  describe '#receive_order' do
+    it 'should receive instruction to make drink with sugar' do
+      # GIVEN
+      drink = 'T'
+      sugar = 1
+
+      # WHEN
+      result = CoffeeCart.new.receive_order(drink, sugar)
+      puts result
+
+      # THEN
+      expect(result).to eq('T:1:0')
+    end
+
+    it 'should receive instruction to make drinks with no sugar' do
+      # GIVEN
+      drink = 'H'
+      sugar = 0
+
+      # WHEN
+      result = CoffeeCart.new.receive_order(drink, sugar)
+      puts result
+
+      # THEN
+      expect(result).to eq('H::')
+    end
+
+    it 'should receive instruction to make drinks with two sugar and add a stick' do
+      # GIVEN
+      drink = 'C'
+      sugar = 2
+
+      # WHEN
+      result = CoffeeCart.new.receive_order(drink, sugar)
+      puts result
+
+      # THEN
+      expect(result).to eq('C:2:0')
+    end
+  end
+
+  describe '#deliver_message' do
+    it 'should receive a message with a drink order' do
+      # GIVEN
+
+      # WHEN
+      result = CoffeeCart.new.deliver_message('Hello')
+
+      # THEN
+      expect(result).to eq('M:' + 'Hello')
+    end
+  end
+end
